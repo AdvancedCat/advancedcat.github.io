2c654dc7cd3ac7ae222225d5f58949356928557e
Flame <xinhong@anjuke.com>
2018-02-10 16:17:04 +0800

    Site updated from HexoBlog - 2018-02-10 16:17:04

--- scripts/main.js
+++ scripts/main.js
@@ -56,12 +56,20 @@
 
 	var _tag = __webpack_require__(5);
 
-	console.info('hexo-theme-archer: v201701020');
+	var _preview = __webpack_require__(7);
+
+	var _preview2 = _interopRequireDefault(_preview);
+
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+	//onsole.info('hexo-theme-archer: v201701020');
 	(0, _init.init)();
 	(0, _toggleHeader.toggleHeader)();
 	(0, _sidebar.sidebarInit)();
 	(0, _tag.initTag)();
 	(0, _mobile.initMobile)();
+
+	_preview2.default.initBigImg();
 
 /***/ }),
 /* 1 */
@@ -612,6 +620,76 @@
 	};
 
 	exports.archerUtil = archerUtil;
+
+/***/ }),
+/* 7 */
+/***/ (function(module, exports, __webpack_require__) {
+
+	'use strict';
+
+	var _browser = __webpack_require__(8);
+
+	var _browser2 = _interopRequireDefault(_browser);
+
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+	var Preview = {
+	    initBigImg: function initBigImg() {
+	        if (true) {
+	            var imgDoms = $('.post-page').find('img');
+	            imgDoms.on('click', function (e) {
+	                var $img = $(this),
+	                    imgSrc = $img.prop('src');
+
+	                //onsole.log('点击了图片', imgSrc)
+	                generateBigImg(imgSrc);
+	            });
+
+	            $('body').on('click', '.preview-mask', function (e) {
+	                var $this = $(this);
+	                $this.remove();
+	            });
+	        }
+	    }
+
+	};
+
+	function generateBigImg(src) {
+
+	    var mask = '\n            <div class="preview-mask">\n                <img src="PREVIEW_SRC" alt="\u9884\u89C8\u56FE\u7247">\n            </div>\n        ';
+	    mask = mask.replace('PREVIEW_SRC', src);
+
+	    $('body').append($(mask));
+	}
+
+	module.exports = Preview;
+
+/***/ }),
+/* 8 */
+/***/ (function(module, exports) {
+
+	'use strict';
+
+	var browser = {
+	    versions: function () {
+	        var u = window.navigator.userAgent;
+	        return {
+	            trident: u.indexOf('Trident') > -1, //IE内核
+	            presto: u.indexOf('Presto') > -1, //opera内核
+	            webKit: u.indexOf('AppleWebKit') > -1, //苹果、谷歌内核
+	            gecko: u.indexOf('Gecko') > -1 && u.indexOf('KHTML') == -1, //火狐内核
+	            mobile: !!u.match(/AppleWebKit.*Mobile.*/), //是否为移动终端
+	            ios: !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/), //ios终端
+	            android: u.indexOf('Android') > -1 || u.indexOf('Linux') > -1, //android终端或者uc浏览器
+	            iPhone: u.indexOf('iPhone') > -1 || u.indexOf('Mac') > -1, //是否为iPhone或者安卓QQ浏览器
+	            iPad: u.indexOf('iPad') > -1, //是否为iPad
+	            webApp: u.indexOf('Safari') == -1, //是否为web应用程序，没有头部与底部
+	            weixin: u.indexOf('MicroMessenger') == -1 //是否为微信浏览器
+	        };
+	    }()
+	};
+
+	module.exports = browser;
 
 /***/ })
 /******/ ]);
